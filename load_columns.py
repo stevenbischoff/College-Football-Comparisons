39c8bd47f0a7477cf11859c25fcece7184dce85b
Steven Bischoff <61480100+stevenbischoff@users.noreply.github.com>
2024-01-22 15:28:00 -0600

    Update load_columns.py

--- load_columns.py
+++ load_columns.py
@@ -1,19 +1,19 @@
 import pickle
 
-with open('data/o_normal_columns.pkl', 'rb') as f:
+with open('static/o_normal_columns.pkl', 'rb') as f:
     o_normal_columns = pickle.load(f)
 
-with open('data/o_advanced_columns.pkl', 'rb') as f:
+with open('static/o_advanced_columns.pkl', 'rb') as f:
     o_advanced_columns = pickle.load(f)
 
-with open('data/d_normal_columns.pkl', 'rb') as f:
+with open('static/d_normal_columns.pkl', 'rb') as f:
     d_normal_columns = pickle.load(f)
 
-with open('data/d_advanced_columns.pkl', 'rb') as f:
+with open('static/d_advanced_columns.pkl', 'rb') as f:
     d_advanced_columns = pickle.load(f)
 
-with open('data/other_columns.pkl', 'rb') as f:
+with open('static/other_columns.pkl', 'rb') as f:
     other_columns = pickle.load(f)
 
-with open('data/X_columns.pkl', 'rb') as f:
+with open('static/X_columns.pkl', 'rb') as f:
     X_columns = pickle.load(f)
