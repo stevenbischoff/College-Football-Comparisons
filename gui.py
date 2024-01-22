b1938e34ecccb8d64feb050a0ad7a667ed4419f4
Steven Bischoff <61480100+stevenbischoff@users.noreply.github.com>
2024-01-22 15:39:09 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -99,6 +99,15 @@ schools = sorted(team_df['school'].unique())
 nn = NearestNeighbors(n_neighbors=n_comps+1).fit(df_transformed)
 
 ### APP ###
+st.markdown(f"""
+    <style>
+        .reportview-container .main .block-container{{
+            padding-top: {padding_top}rem;
+        }}
+    </style>""",
+    unsafe_allow_html=True,
+)
+
 st.header('College Football: Most Statistically Similar FBS Teams 2004-2023')
 st.write("Select a school and a year to view the team's closest statistical comparisons")
 left_column, mid_column, right_column = st.columns([0.4,0.4,0.2])
