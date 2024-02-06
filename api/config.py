66a789d33933a26df47e1d1aad6b622ec469010f
Steven Bischoff <61480100+stevenbischoff@users.noreply.github.com>
2024-02-06 07:51:32 -0600

    Delete gui_test

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 import cfbd
 
 configuration = cfbd.Configuration()
-configuration.api_key['Authorization'] = 'i6oXbNyzWrsdKES2ni5Bxx1YZrWkDFyCIPAo5W0Fer2tz9mW6L5/1viV3qhJWOAN'
+configuration.api_key['Authorization'] = 'INSERT YOUR KEY HERE' # !!!
 configuration.api_key_prefix['Authorization'] = 'Bearer'
